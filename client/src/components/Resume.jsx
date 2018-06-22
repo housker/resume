d2b7008368e7ebae0c3d38b042080f2b4bc3f97d
Adelle Housker <housker@gmail.com>
2018-06-22 10:37:58 -0500

    bootstrap responsive embed

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -4,8 +4,10 @@ import resume from '../assets/Housker2016.pdf';
 
 function Resume(props) {
   return (
-    <div>
-<embed src={resume} width="800px" height="2100px" />
+    <div className="embed-responsive embed-responsive-16by9">
+      <object className="embed-responsive-item" data={resume} type="application/pdf" internalinstanceid="9" title="">
+        <p>Your browser isn't supporting embedded pdf files. You can download the file</p>
+      </object>
     </div>
   );
 }
