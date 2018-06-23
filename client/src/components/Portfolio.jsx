ce990c6b160a78e96ded16d78c5edb800391e477
Adelle Housker <housker@gmail.com>
2018-06-23 15:57:41 -0500

    portfolio maps text

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -7,30 +7,89 @@ import windowicon from '../assets/windowicon.svg';
 class Portfolio extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+
+      test: [
+        {title: 'title1', description: 'description1', image: 'https://picsum.photos/500/400/?random'},
+        {title: 'title2', description: 'description2', image: 'https://picsum.photos/500/400/?random'},
+        {title: 'title3', description: 'description3', image: 'https://picsum.photos/500/400/?random'},
+        {title: 'title4', description: 'description4', image: 'https://picsum.photos/500/400/?random'},
+        {title: 'title5', description: 'description5', image: 'https://picsum.photos/500/400/?random'},
+        {title: 'title6', description: 'description6', image: 'https://picsum.photos/500/400/?random'}
+      ]
+
+    };
+    this.light = this.light.bind(this);
+    this.myThing;
+  }
+
+  componentWillMount() {
+        this.myThing = this.state.test.slice(0, 2).map((item, i) => {
+            <div key={i} className="panel" onMouseOver={this.light}>
+              <div id="text">{item.title}</div>
+              <img className="port-img" img-responsive src={item.image}/>
+            </div>
+          })
   }
 
   componentDidMount() {
+    let map = this.state.test.slice(0, 2).map((project, i) => 'my' + project.title)
+    console.log('map: ', map)
+    console.log('this.state.test.slice(0, 2)', this.state.test.slice(0, 2))
+            this.myThing = this.state.test.slice(0, 2).map((item, i) => {
+            <div key={i} className="panel" onMouseOver={this.light}>
+              <div id="text">{item.title}</div>
+              <img className="port-img" img-responsive src={item.image}/>
+            </div>
+          })
+    console.log('this.myThing: ', this.myThing)
+
+  }
+
+  light() {
+    console.log('I am lightning!')
+
   }
 
   render() {
+    console.log('this.state.test: ', this.state.test)
 
 
   return (
     <div>
       <div className="row port-row d-flex-row">
+
+
+
         <div className="col port-col col-lg-4 col-md-6 col-sm-12">
-          <img className="port-img" img-responsive src="https://picsum.photos/500/400/?random"/>
-          <img className="port-img" img-responsive src="https://picsum.photos/500/400/?random"/>
+          {this.state.test.slice(0, 2).map((project, i) => (
+            <div className="panel">
+              <div id="text">{project.title}</div>
+              <img className="port-img" img-responsive src={project.image}/>
+            </div>
+          ))}
         </div>
+
         <div className="col port-col col-lg-4 col-md-6 col-sm-12">
-          <img className="port-img" img-responsive src="https://picsum.photos/500/400/?random"/>
-          <img className="port-img" img-responsive src="https://picsum.photos/500/400/?random"/>
+          {this.state.test.slice(2, 4).map((project, i) => (
+            <div className="panel">
+              <div id="text">{project.title}</div>
+              <img className="port-img" img-responsive src={project.image}/>
+            </div>
+          ))}
         </div>
+
         <div className="col port-col col-lg-4 col-md-6 col-sm-12">
-          <img className="port-img" img-responsive src="https://picsum.photos/500/400/?random"/>
-          <img className="port-img" img-responsive src="https://picsum.photos/500/400/?random"/>
+          {this.state.test.slice(4, 6).map((project, i) => (
+            <div className="panel">
+              <div id="text">{project.title}</div>
+              <img className="port-img" img-responsive src={project.image}/>
+            </div>
+          ))}
         </div>
+
+
+
       </div>
     </div>
   );
