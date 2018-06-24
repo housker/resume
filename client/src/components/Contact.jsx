295b179a091a2a8b9974c2b6a3d0ce1f69dee548
Adelle Housker <housker@gmail.com>
2018-06-23 22:05:05 -0500

    (feature) resume header links

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Resume from './Resume.jsx';
+var images = require.context('../assets/icons', false, /\.(png|jpg|gif|svg)$/);
 
 function Contact(props) {
   return (
-
-
-    <div>
-
-
-<h1 className="d-inline">Contact</h1><span>LinkedIn</span>Github<span></span>Twitter<span></span>
-
-<Resume />
-
-
-
-
-
-
-
+  <div>
+    <div className="d-flex justify-content-center">
+      <a href="#"><img className="contact-icon m-2 p-0" height="30" width="30" src={images('./iconmail.svg')} onClick={() => window.open('mailto:housker@gmail.com')} /></a>
+      <a href="https://www.linkedin.com/in/adelle-housker-a0990342/"><img className="m-2 p-0" height="30" width="30" src={images('./iconli.png')} /></a>
+      <a href="https://github.com/housker"><img className="m-2 p-1" height="30" width="30" src={images('./icongh.png')} /></a>
+      <a href="https://twitter.com/AHousker"><img className="m-2 p-1" height="30" width="30" src={images('./icontwitter.png')} /></a>
     </div>
+    <Resume />
+  </div>
   );
 }
 
 export default Contact;
-
-
-//get icons for each span and make them links
--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import dashboard from '../assets/dashboard.png';
 import criboard from '../assets/criboard.png';
 import windowicon from '../assets/windowicon.svg';
+var images = require.context('../assets/thumbnails', false, /\.(png|jpg|gif)$/);
 
 class Portfolio extends React.Component {
   constructor(props) {
@@ -10,16 +11,17 @@ class Portfolio extends React.Component {
     this.state = {
       highlight: false,
       test: [
-        {index: 0, title: 'title1', description: 'description1', image: 'https://picsum.photos/500/400/?random'},
-        {index: 1, title: 'title2', description: 'description2', image: 'https://picsum.photos/500/400/?random'},
-        {index: 2, title: 'title3', description: 'description3', image: 'https://picsum.photos/500/400/?random'},
-        {index: 3, title: 'title4', description: 'description4', image: 'https://picsum.photos/500/400/?random'},
-        {index: 4, title: 'title5', description: 'description5', image: 'https://picsum.photos/500/400/?random'},
-        {index: 5, title: 'title6', description: 'description6', image: 'https://picsum.photos/500/400/?random'}
+        {index: 0, title: 'Criboard', description: 'Developed through github version control, this represents the initial stages of a project aimed to make apartment living and management more collaboartive for both tenants and landlords. Criboard is a full stack app using Sequelize for database management, Passport.js for user authentication, and Multer for form parsing. The client utilizes RESTful APIs to populate maps and queries. React Router and React are used for routing, managing state, and rendering.', image: './tncriboard.png', link: 'https://safe-tundra-98409.herokuapp.com/'},
+        {index: 1, title: 'Financial Dashboard', description: 'This personal finance dashboard is a mock-up demonstrating UI designed for a responsive, client-centric, visually rich interactive experience. It utilizes React for state management and rendering, Bootstrap for adaptable styling, and Chart.js to dynamically communicate raw data into a relatabale, easily accessible format.', image: './tndashboard.png', link: 'https://safe-tundra-98409.herokuapp.com/'},
+        {index: 2, title: 'title3', description: 'description3', image: './tndashboard.png', link: 'https://safe-tundra-98409.herokuapp.com/'},
+        {index: 3, title: 'title4', description: 'description4', image: './tndashboard.png', link: 'https://safe-tundra-98409.herokuapp.com/'},
+        {index: 4, title: 'title5', description: 'description5', image: './tndashboard.png', link: 'https://safe-tundra-98409.herokuapp.com/'},
+        {index: 5, title: 'title6', description: 'description6', image: './tndashboard.png', link: 'https://safe-tundra-98409.herokuapp.com/'}
       ]
     };
     this.light = this.light.bind(this);
     this.resume = this.resume.bind(this);
+    this.test = (<p>This personal finance dashboard is a mock-up demonstrating UI designed for a responsive, client-centric, visually rich interactive experience. It utilizes React for state management and rendering, Bootstrap for adaptable styling, and Chart.js to dynamically communicate raw data into a relatabale, easily accessible format. Make sure to click the <img src={windowicon} height="17"></img> icon to view the full page.</p>)
   }
 
   light(i) {
@@ -38,8 +40,8 @@ class Portfolio extends React.Component {
           {this.state.test.slice(0, 2).map(project => (
             <div key={project.index} className="panel" onMouseOver={() => this.light(project.index)} onMouseLeave={this.resume}>
               {this.state.highlight === project.index && <div className="layer" />}
-              {this.state.highlight === project.index ? (<div id="overlay" className="text">{project.description}</div>) : (<div className="text">{project.title}</div>)}
-              <img className="port-img" img-responsive src={project.image}/>
+              {this.state.highlight === project.index ? (<a id="overlay" className="text" href={project.link}>{project.description.slice(0)}</a>) : (<h3 className="text">{project.title}</h3>)}
+              <img className="port-img" img-responsive src={images(project.image)} height="400" width="500"/>
             </div>
           ))}
         </div>
@@ -47,8 +49,8 @@ class Portfolio extends React.Component {
           {this.state.test.slice(2, 4).map(project => (
             <div key={project.index} className="panel" onMouseOver={() => this.light(project.index)} onMouseLeave={this.resume}>
               {this.state.highlight === project.index && <div className="layer" />}
-              {this.state.highlight === project.index ? (<div id="overlay" className="text">{project.description}</div>) : (<div className="text">{project.title}</div>)}
-              <img className="port-img" img-responsive src={project.image}/>
+              {this.state.highlight === project.index ? (<a id="overlay" className="text" href={project.link}>{project.description}</a>) : (<h3 className="text">{project.title}</h3>)}
+              <img className="port-img" img-responsive src={images(project.image)} height="400" width="500"/>
             </div>
           ))}
         </div>
@@ -56,8 +58,8 @@ class Portfolio extends React.Component {
           {this.state.test.slice(4, 6).map(project => (
             <div key={project.index} className="panel" onMouseOver={() => this.light(project.index)} onMouseLeave={this.resume}>
               {this.state.highlight === project.index && <div className="layer" />}
-              {this.state.highlight === project.index ? (<div id="overlay" className="text">{project.description}</div>) : (<div className="text">{project.title}</div>)}
-              <img className="port-img" img-responsive src={project.image}/>
+              {this.state.highlight === project.index ? (<a id="overlay" className="text" href={project.link}>{project.description}</a>) : (<h3 className="text">{project.title}</h3>)}
+              <img className="port-img" img-responsive src={images(project.image)} height="400" width="500"/>
             </div>
           ))}
         </div>
