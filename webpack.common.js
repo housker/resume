551a6dd42f1beaf037834fb6d0d3b6f7f9cb3704
Adelle Housker <housker@gmail.com>
2018-07-09 14:55:19 -0500

    configured webpack for production build

---
+++ webpack.common.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(jsx)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(png|svg|jpg|gif|pdf|ico)$/, use: ['file-loader']}
+    ]
+  }
+}
