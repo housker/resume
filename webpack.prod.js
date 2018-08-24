bd6e327275c697b3c78e32145e67c225173b7b42
housker <housker@gmail.com>
2018-08-24 17:50:16 -0400

    performance optimization

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
   const webpack = require('webpack');
   const merge = require('webpack-merge');
   const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+  const ClosureCompilerPlugin = require('webpack-closure-compiler');
   const common = require('./webpack.common.js');
 
   module.exports = merge(common, {
@@ -10,6 +11,19 @@
       new UglifyJSPlugin({
         sourceMap: true
       }),
+
+      // new ClosureCompilerPlugin({
+      //   compiler: {
+      //     jar: 'path/to/your/custom/compiler.jar', //optional
+      //     language_in: 'ECMASCRIPT6',
+      //     language_out: 'ECMASCRIPT5',
+      //     compilation_level: 'ADVANCED'
+      //   },
+      //   concurrency: 3,
+      // })
+
+
+
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production')
       })
